abe2f7da54fbbc65206b2af370d39b753e3cb2a4
Isabella Silvers <isilvers521@gmail.com>
2024-12-01 17:57:06 -0800

    Finished matrices.cpp and particle.cpp

--- Matrices.cpp
+++ Matrices.cpp
@@ -90,16 +90,38 @@ namespace Matrices
         }
         return os;
     }
-    RotationMatrix::RotationMatrix(double theta) :Matrix(2, 2)
-    {
 
+    RotationMatrix::RotationMatrix(double theta) : Matrix(2, 2)
+    {
+        a.at(0).at(0) = cos(theta);
+        a.at(0).at(1) = -sin(theta);
+        a.at(1).at(0) = sin(theta);
+        a.at(1).at(1) = cos(theta);
     }
+    
     ScalingMatrix::ScalingMatrix(double scale) : Matrix(2, 2)
     {
-    
+        a.at(0).at(0) = scale;
+        a.at(0).at(1) = 0;
+        a.at(1).at(0) = 0;
+        a.at(1).at(1) = scale;
     }
+
     TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
     {
-
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < nCols; j++)
+            {
+                if (i == 0)
+                {
+                    a.at(i).at(j) = xShift;
+                }
+                else
+                {
+                    a.at(i).at(j) = yShift;
+                }
+            }
+        }
     }
 }
--- Particle.cpp
+++ Particle.cpp
@@ -1,16 +1,75 @@
 #include "Particle.h"
 Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition):m_A(2, numPoints)
 {
+    m_ttl = TTL;
+    m_numPoints = numPoints;
+    m_radiansPerSec = ((float)rand() / RAND_MAX) * M_PI;
 
+    m_cartesianPlane.setCenter(0, 0);
+    m_cartesianPlane.setSize(target.getSize().x, (-1.0) * target.getSize().y);
+    m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
+
+    m_vx = rand() % 400 + 100;
+    //make m_vx randomly positive or negative
+    if (rand() % 2 != 0)
+    {
+        m_vx *= -1;
+    }
+    m_vy = rand() % 400 + 100;
+
+    //set m_color1 to white
+    m_color1.r = 255;
+    m_color1.g = 255;
+    m_color1.b = 255;
+
+    //set m_color2 to random
+    m_color2.r = rand() % 256;
+    m_color2.g = rand() % 256;
+    m_color2.b = rand() % 256;
+
+    double theta = (((float)rand() / RAND_MAX) * M_PI) / 2;
+    double dTheta = 2 * M_PI / (numPoints - 1);
+
+    for (int j = 0; j < numPoints; j++)
+    {
+        double r, dx, dy;
+        r = rand() % 60 + 20;
+        dx = r * cos(theta);
+        dy = r * sin(theta);
+        m_A(0, j) = m_centerCoordinate.x + dx;
+        m_A(1, j) = m_centerCoordinate.y + dy;
+        theta += dTheta;
+    }
 }
+
 void Particle::draw(RenderTarget& target, RenderStates states) const
 {
+    VertexArray lines(TriangleFan, m_numPoints + 1);
+    Vector2f center(target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane));
+    lines[0].position = center;
+    lines[0].color = (m_color1, m_color2);
 
+    for (int j = 1; j < m_numPoints + 1; j++)
+    {
+        Vector2f coords(m_A(0, j - 1), m_A(1, j - 1));
+        lines[j].position = Vector2f(target.mapCoordsToPixel(coords, m_cartesianPlane));
+    }
+
+    target.draw(lines);
 }
+
 void Particle::update(float dt)
 {
-
+    m_ttl -= dt;
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE);
+    float dx, dy;
+    dx = m_vx * dt;
+    m_vy -= G * dt;
+    dy = m_vy * dt;
+    translate(dx, dy);
 }
+
 bool Particle::almostEqual(double a, double b, double eps)
 {
 	return fabs(a - b) < eps;
@@ -154,15 +213,29 @@ void Particle::unitTests()
 
     cout << "Score: " << score << " / 7" << endl;
 }
+
 void Particle::rotate(double theta)
 {
-
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R * m_A;
+    translate(temp.x, temp.y);
 }
+
 void Particle::scale(double c)
 {
-
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+    translate(temp.x, temp.y);
 }
+
 void Particle::translate(double xShift, double yShift)
 {
-
+    TranslationMatrix T(xShift, yShift, m_A.getCols());
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
 }
